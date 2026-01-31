676df814ba44b24c764b66482019c54dcdf931e7
Victoria Gao <victoriagaojing@gmail.com>
2026-01-31 16:58:26 -0500

    send access token

--- frontend/app/auth/callback/route.ts
+++ frontend/app/auth/callback/route.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@/app/lib/supabaseClient';
+import { createClient } from '@/app/lib/supabaseServer';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const code = requestUrl.searchParams.get('code');
 
   if (code) {
-    const supabase = createClient();
+    const supabase = await createClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
 
